4b6f5f56c4f95c23f2f073a9320854e522103ced
Az1yaT <jeenbek0vv.06@gmail.com>
2022-08-20 22:13:37 +0600

    We added new method

--- Control_work/Program.cs
+++ Control_work/Program.cs
@@ -5,3 +5,14 @@
 // 1. Задаем одномерный массив строк: 
 
 string[] arrString = { "kdifhg", "12f", "df3", ")_*", "%5^&" };
+
+// 2. Выводим на печать исходный массив: 
+void PrintArray(string[] arrString)
+{
+    for (int i = 0; i < arrString.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < arrString.Length - 1) Console.Write(arrString[i] + ", ");
+        else Console.Write(arrString[i] + "]");
+    }
+}
